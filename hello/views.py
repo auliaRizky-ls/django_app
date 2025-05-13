360964bd4c6725b18727305da8b71bc035c729f9
rizky <aulia.rizky.ls.tis.2025@gmail.com>
2025-05-13 15:25:16 +0900

    Question 2-2 done

--- hello/views.py
+++ hello/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello World!") #Question 2-1
+def index(request, firstname, lastname):
+    #return HttpResponse("Hello World!") #Question 2-1
+    return HttpResponse(f"Hello, {firstname} {lastname}") #Question 2-2
