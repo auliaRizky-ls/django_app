af6940f17a7aa5d0b5b783b10c1e605f9fdc7044
rizky <aulia.rizky.ls.tis.2025@gmail.com>
2025-05-19 13:26:55 +0900

    chapter 3-4 done

--- hello/forms.py
+++ hello/forms.py
@@ -1,8 +1,17 @@
 from django import forms
 from .models import Friend
 
+class FriendForm(forms.ModelForm):
+    class Meta:
+        model = Friend
+        fields = ['name', 'mail', 'gender', 'age', 'birthday']
+
 class HelloForm(forms.Form):
-    id = forms.IntegerField(label = "ID", min_value = 1, max_value = Friend.objects.all().count())
+    name = forms.CharField(label = "Name", widget = forms.TextInput(attrs={"class":"form-control"}))
+    mail = forms.EmailField(label = "Email", widget = forms.EmailInput(attrs={"class":"form-control"}))
+    gender = forms.BooleanField(label = "Gender", required = False, widget = forms.CheckboxInput(attrs={"class":"form-check"}))
+    age = forms.IntegerField(label = "Age", widget = forms.NumberInput(attrs={"class":"form-control"}))
+    birthday = forms.DateField(label = "Birthday", widget = forms.DateInput(attrs={"class":"form-control"}))
 
 class SessionForm(forms.Form):
     session = forms.CharField(label = "session", required = False, widget = forms.TextInput(attrs={"class":"form-control"}))
